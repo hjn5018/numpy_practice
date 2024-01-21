e90a8f52ac3406143e489f1ad1e2fd4bc28765eb
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-01-21 10:08:29 +0900

    Create numpy_practice.py

---
+++ numpy_practice.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 09:05:35 2024
+
+@author: hjn50
+"""
+
+import numpy as np
+
+# help(np.sort)
+
+# np.lookfor('keyword')
+
+# from numpy import doc
+
+# help(np.doc.TOPIC)
+
+# np.*cos*?
+
+# print(np.cos?)
